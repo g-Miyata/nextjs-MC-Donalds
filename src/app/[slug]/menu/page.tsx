03d39c7b66211ca9e5514768703eb685f347b427
Guilherme Miyata <miyatagui120600@gmail.com>
2025-03-02 14:57:28 -0300

    feat: add inital restaurant and menu pages, and funcion get restaurant

---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,38 @@
+import { notFound } from "next/navigation";
+
+import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
+
+import RestaurantHeader from "./components/header";
+
+interface RestaurantMenuPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
+}
+
+const isConsumptionMethodValid = (ConsumptionMethod: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(
+    ConsumptionMethod.toLocaleUpperCase(),
+  );
+};
+
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { consumptionMethod } = await searchParams;
+  const { slug } = await params;
+  if (!isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+  const restaurant = await getRestaurantBySlug(slug);
+  if (!restaurant) {
+    return notFound();
+  }
+  return (
+    <div>
+      <RestaurantHeader restaurant={restaurant} />
+    </div>
+  );
+};
+
+export default RestaurantMenuPage;
