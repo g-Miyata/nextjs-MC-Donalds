d9980ab9d1bb295c12f8c153d9e3df0e60ef89fa
Guilherme Miyata <miyatagui120600@gmail.com>
2025-03-10 11:22:18 -0300

    feat: add orders page

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { ConsumptionMethod } from "@prisma/client";
+import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
@@ -51,6 +52,8 @@ const createOrder = async (input: CreateOrderInput) => {
       restaurantId: restaurant.id,
     },
   });
+  redirect(`/${input.slug}/orders`);
 };
+
 
 export default createOrder;
---
+++ src/app/[slug]/orders/components/order-item.tsx
@@ -0,0 +1,5 @@
+const OrderItem = () => {
+  return <h1>OrderItem</h1>;
+};
+
+export default OrderItem;
